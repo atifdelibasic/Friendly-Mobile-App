a6854c8373fc87ce8194f377f0f3e807d63837de
unknown <atif.delibasic@edu.fit.ba>
2024-07-06 21:43:00 +0200

    add dynamic appurl

--- lib/utility/app_url.dart
+++ lib/utility/app_url.dart
@@ -1,10 +1,11 @@
+import 'dart:io';
+
 class AppUrl {
-  // static const String baseUrl = 'http://10.0.2.2:7169';
 
-  static const String baseUrl = 'http://localhost:7169';
+  static  String baseUrl = Platform.isAndroid? 'http://10.0.2.2:7169': 'http://localhost:7169';
 
-  static const String login = '$baseUrl/user/login';
-  static const String register = '$baseUrl/user/register';
-  static const String forgotPassword = '$baseUrl/forgot-password';
-  static const String likePost = '$baseUrl/like';
+  static  String login = '$baseUrl/user/login';
+  static  String register = '$baseUrl/user/register';
+  static  String forgotPassword = '$baseUrl/forgot-password';
+  static  String likePost = '$baseUrl/like';
 }
